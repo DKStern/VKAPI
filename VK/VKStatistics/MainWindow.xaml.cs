6d30369d5abcc30d66143cc26b59441a3e386e5a
Владимир Ермолаев <Ermolaev.vova@hotmail.com>
2020-01-12 23:09:07 +0500

    Заготовка

---
+++ VK/VKStatistics/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace VKStatistics
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
