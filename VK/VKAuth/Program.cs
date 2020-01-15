4a71d3645fb2695a5acd17cf1d40924f16a35558
Владимир Ермолаев <Ermolaev.vova@hotmail.com>
2020-01-15 21:04:25 +0500

    Консоль для получения токена к странице с 2х факторной аунтификацией

---
+++ VK/VKAuth/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+namespace VKAuth
+{
+    public class Program
+    {
+        private static readonly ulong AppId = 7282889;
+        private static string login;
+        private static string password;
+
+        static void Main(string[] args)
+        {
+            var api = new VkApi();
+
+            Console.Write("Введите логин: ");
+            login = Console.ReadLine();
+            Console.Write("Введите пароль: ");
+            password = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password))
+            {
+
+                api.Authorize(new ApiAuthParams
+                {
+                    ApplicationId = AppId,
+                    Login = login,
+                    Password = password,
+                    Settings = Settings.All,
+                    TwoFactorAuthorization = () =>
+                    {
+                        Console.Write("Введите код: ");
+                        return Console.ReadLine();
+                    }
+                });
+
+                Console.WriteLine(api.Token);
+                using (var sw = new StreamWriter("AccesToken.txt", false))
+                {
+                    sw.WriteLine(api.Token);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ VK/VKStatistics/Model/User.cs
@@ -0,0 +1,22 @@
+﻿namespace VKStatistics.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// Id пользователя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя
+        /// </summary>
+        public string LastName { get; set; }
+
+        public User() {}
+    }
+}
